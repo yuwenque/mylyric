1c20cce4b5bc08c21964021d92343a912139f6e2
余文雀 <277825722@qq.com>
2018-09-05 00:15:02 +0800

    test fixed

---
+++ src/main/java/com/ares/Downloader.kt
@@ -0,0 +1,77 @@
+package com.ares
+
+import com.ares.entity.Actress
+import com.ares.http.AppConstants
+import com.ares.http.ArtworkApi
+import com.ares.http.RetrofitServiceManager
+import io.reactivex.Scheduler
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+
+import java.io.*
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ThreadPoolExecutor
+
+object Downloader {
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val artworkApi = RetrofitServiceManager.getManager().create(AppConstants.URL.ARTWORK_URL, ArtworkApi::class.java)
+
+        artworkApi.getActressList(1).subscribeOn(Schedulers.io())
+                .subscribe({ actresses ->
+                    println("actresses.size = " + actresses.size)
+
+                    val service = Executors.newFixedThreadPool(actresses.size)
+
+                    val path = "/Users/yuwenque/Downloads/actresses"
+                    val rootFile = File(path)
+                    println("rootFile .exist = " + rootFile.exists())
+                    for (actress in actresses) {
+                        service.submit { Downloader.downloadPicture(actress.avatar, path + "/" + actress.name + ".jpg") }
+                    }
+                }) { throwable -> throwable.printStackTrace() }
+
+        try {
+            Thread.sleep(200000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    //链接url下载图片
+    private fun downloadPicture(imageUrl: String, filePath: String) {
+        var url: URL? = null
+
+        try {
+            url = URL(imageUrl)
+            val dataInputStream = DataInputStream(url.openStream())
+
+            val fileOutputStream = FileOutputStream(File(filePath))
+            val output = ByteArrayOutputStream()
+
+            val buffer = ByteArray(1024)
+            var length=  dataInputStream.read(buffer)
+
+
+            while (length>0){
+                length = dataInputStream.read(buffer)
+                output.write(buffer, 0, length)
+
+            }
+            fileOutputStream.write(output.toByteArray())
+            dataInputStream.close()
+            fileOutputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+}
---
+++ src/main/java/com/ares/http/ArtworkApi.kt
@@ -0,0 +1,28 @@
+package com.ares.http
+
+import com.ares.entity.Actress
+import com.ares.entity.ActressDetail
+import com.ares.entity.BaseSearchItem
+import com.ares.entity.MovieSearchItem
+import io.reactivex.Observable
+import retrofit2.http.Field
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ArtworkApi {
+
+
+    @GET("/artwork/actresses/{page}")
+    fun getActressList(@Path("page") page: Int): Observable<List<Actress>>
+
+    @GET("/artwork/actressInfo")
+    fun getActressDetail(@Query("id") id: String): Observable<ActressDetail>
+
+    @GET("/artwork/search/{keyword}/{page}")
+    fun getSearchList(@Path("keyword") keyword: String, @Path("page") page: Int, @Query(value = "type") type: Int): Observable<List<BaseSearchItem>>
+
+
+    @GET("/artwork/{code}")
+    fun searchArtWork(@Path("code") code: String): Observable<MovieSearchItem>
+}
