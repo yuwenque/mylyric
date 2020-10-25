c5c5ce40ed1a5533c26dd0cf0abb057ab7adce37
yuwenque <277825722@qq.com>
2020-10-25 13:52:54 +0800

    更新脚本

--- restartapp.sh
+++ restartapp.sh
@@ -2,4 +2,4 @@
 git pull
 mvn package
 kill `lsof -t -i:9595`
-nohup java -jar /usr/local/src/mylyric/target/lyric-0.0.1-SNAPSHOT.jar params1 > nohup.out 2>&1 & 
+nohup java -jar target/lyric-0.0.1-SNAPSHOT.jar params1 > nohup.out 2>&1 & 
